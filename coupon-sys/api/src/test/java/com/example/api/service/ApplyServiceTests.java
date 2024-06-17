ac72b6cbc41786a44d64051659baab5935cf5300
YouchanKim <dbcks4646@gmail.com>
2024-06-18 00:05:33 +0900

    Thread를 활용하여 여러번 서비스 호출

--- coupon-sys/api/src/test/java/com/example/api/service/ApplyServiceTests.java
+++ coupon-sys/api/src/test/java/com/example/api/service/ApplyServiceTests.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
@@ -22,6 +26,32 @@ class ApplyServiceTests {
         applyService.apply(1L);
         long count = couponRepository.count();
         assertThat(count).isEqualTo(1);
+    }
+
+    @Test
+    public void 여러번응모() throws InterruptedException {
+        int threadCount = 1000;
+        //  정렬을 도와주는 라이브러리
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 모든 작업이 종료될때 까지 기다려줌
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for(int i = 0; i < threadCount; i++){
+            long userId = i;
+            executorService.submit(() -> {
+                try{
+
+                    applyService.apply(userId);
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
 
     }
 
